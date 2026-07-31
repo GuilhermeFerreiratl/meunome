91a7d7b09717529e53176f291eeab65e3f9275b0
Guilherme Ferreira <guuiivickytl@gmail.com>
2026-07-31 16:05:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {});
+const CACHE = "meupg-v1";
+const URLS = [
+  "/meunome/",
+  "/meunome/index.html",
+  "/meunome/style.css",
+  "/meunome/script.js",
+  "/meunome/manifest.json",
+  "/meunome/logo.png",
+  "/meunome/icon-192.png",
+  "/meunome/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
